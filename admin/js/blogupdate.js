411154b14e9d7598592659615e84a21e69e8b33e
khaticaa <128252286+khaticaa@users.noreply.github.com>
2024-02-11 15:33:04 +0400

    shop update and delete okey

--- admin/js/blogupdate.js
+++ admin/js/blogupdate.js
@@ -1,50 +1,95 @@
+// let titleInput = document.querySelector(".title");
+// let priceInput = document.querySelector(".price");
+// let photoInput = document.querySelector(".photo");
+// let addBtn = document.querySelector(".addbtn");
+// let roundedImage = document.querySelector(".rounded-image");
+// let url = `http://localhost:3000/shop/`;
+// let id = new URLSearchParams(window.location.search).get("id")
+
+
+
+
+
+// fetch(url+id)
+// .then(response => response.json())
+// .then(data =>{
+//     titleInput.value = data.name;
+//     roundedImage.src = data.image;
+//     priceInput.value = data.price;
+//     console.log(data)
+// })
+
+
+
+// photoInput.addEventListener('input', (e) => {
+//     let file = e.target.files[0];
+//     if (file) {
+//     //   let reader = new FileReader();
+//       reader.readAsDataURL(file);
+//       reader.URLSearchParams(file);
+//       reader.onload = function () {
+//         roundedImage.src = reader.result;
+//       }
+//     }
+//   })
+//   ;
+
+
+// addBtn.addEventListener("click", (event)=> {
+//     event.preventDefault();
+
+//     let obj = {
+//         name: titleInput.value,
+//         image: roundedImage.src,
+//         price: price.value
+//     }
+//     axios.put(`http://localhost:3000/shop/${id}`, obj)
+//     .then(res => {
+//         window.location = "../shop.html"
+//     })
+// })
+
+
+
 let titleInput = document.querySelector(".title");
 let priceInput = document.querySelector(".price");
 let photoInput = document.querySelector(".photo");
 let addBtn = document.querySelector(".addbtn");
-let roundedImage = document.querySelector(".rounded-image");
+let roundedImage = document.querySelector(".daireimage");
 let url = `http://localhost:3000/shop/`;
-let id = new URLSearchParams(window.location.search).get("id")
+let id = new URLSearchParams(window.location.search).get("id");
 
-
-
-
-
-fetch(url+id)
-.then(response => response.json())
-.then(data =>{
-    titleInput.value = data.name;
-    roundedImage.src = data.image;
-    priceInput.value = data.price;
-    console.log(data)
-})
-
-
+fetch(url + id)
+    .then(response => response.json())
+    .then(data => {
+        titleInput.value = data.name;
+        roundedImage.src = data.image;
+        priceInput.value = data.price;
+        console.log(data);
+    });
 
 photoInput.addEventListener('input', (e) => {
     let file = e.target.files[0];
     if (file) {
-      let reader = new FileReader();
-      reader.readAsDataURL(file);
-    //   reader.URLSearchParams(file);
-      reader.onload = function () {
-        roundedImage.src = reader.result;
-      }
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            roundedImage.src = reader.result;
+        };
     }
-  })
-  ;
+});
 
-
-addBtn.addEventListener("submit", (event)=> {
-    event.preventDefault();
-
+addBtn.addEventListener("click", () => {
     let obj = {
-        name: name.value,
+        name: titleInput.value,
         image: roundedImage.src,
-        price: price.value
-    }
+        price: priceInput.value
+    };
     axios.put(`http://localhost:3000/shop/${id}`, obj)
-    .then(res => {
-        window.location = "./shop.html"
-    })
-})
+        .then(res => {
+            window.location.href = "../shop.html"; 
+        })
+        .catch(error => {
+            console.error('Güncelleme hatası:', error);
+        });
+});
